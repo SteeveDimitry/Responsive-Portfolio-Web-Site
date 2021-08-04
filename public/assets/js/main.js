b3072a6d3d5603c73a52c0471453bcbc82d459e1
Steeve <steevedimitrynana98@gmail.com>
2021-08-04 14:19:58 +0100

    ...

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -127,3 +127,33 @@ let swiperTestimonial = new Swiper('.testimonial__container', {
         }
     }
 });
+
+// Active links
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// Header bg
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is greater than 560 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
