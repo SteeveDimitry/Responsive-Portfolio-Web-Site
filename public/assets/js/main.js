9260a1221e12ba9ac5fa5d27066d74457c0958e6
Steeve <steevedimitrynana98@gmail.com>
2021-08-04 13:49:54 +0100

    content

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -92,3 +92,38 @@ modalCloses.forEach((modalCloses) => {
         })
     })
 })
+
+// Portfolio swiper
+
+let swiperPortfolio = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+});
+
+// Testimonial swiper
+
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
+});
